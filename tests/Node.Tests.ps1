ec27571eaef22fbb5ea667f67d5cf34d3e9562a6
aparnajyothi-y <147696841+aparnajyothi-y@users.noreply.github.com>
2024-07-08 16:23:28 +0530

    Update Node.Tests.ps1

--- tests/Node.Tests.ps1
+++ tests/Node.Tests.ps1
@@ -38,20 +38,21 @@ BeforeAll {
     }
 
   It "cached version is used without downloading" {
-        if ($env:RUNNER_TYPE -eq "GitHub") {
-            # Analyze output of previous steps to check if Node.js was consumed from cache or downloaded
-            $useNodeLogFile = Get-UseNodeLogs
-            $useNodeLogFile | Should -Exist
-            $useNodeLogContent = Get-Content $useNodeLogFile -Raw
-            $useNodeLogContent | Should -Match "Found in cache"
-        } elseif ($env:RUNNER_TYPE -eq "self-hosted") {
+
+       if ($env:RUNNER_TYPE -eq "self-hosted") {
             # Get the installed version of Node.js
             $nodeVersion = Invoke-Expression "node --version"
             # Check if Node.js is installed
             $nodeVersion | Should -Not -BeNullOrEmpty
             # Check if the installed version of Node.js is the expected version
             $nodeVersion | Should -Match $env:VERSION
-        }
+        }else {
+            # Analyze output of previous steps to check if Node.js was consumed from cache or downloaded
+            $useNodeLogFile = Get-UseNodeLogs
+            $useNodeLogFile | Should -Exist
+            $useNodeLogContent = Get-Content $useNodeLogFile -Raw
+            $useNodeLogContent | Should -Match "Found in cache"
+        } 
     }
 
     It "Run simple code" {
